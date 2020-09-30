40d5c7b6a476e22cbfcde28c5b9e56b34b2e66b1
Debarghya Das <72141607+MKBHD-alpha@users.noreply.github.com>
2020-10-01 00:52:29 +0530

    Update RandomGen.py

--- RandomGen.py
+++ RandomGen.py
@@ -3,14 +3,14 @@ import random
 min_value = 1
 max_value = 6
 
-again = True
+again = False
 
 while again:
 
     print(random.randint(min_value, max_value))
 
     another_roll = input('Want to roll the die again? ')
-
+    break
     if another_roll.lower() == 'yes' or another_roll.lower() == 'y':
         continue
     else:
